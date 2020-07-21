ebed950e84df1086cd0073703f927877db43c183
den4kaden4k <54065743+den4kaden4k@users.noreply.github.com>
2020-07-21 18:57:28 +0300

    task1 done

--- task_1.py
+++ task_1.py
@@ -8,3 +8,23 @@
 («Узел доступен», «Узел недоступен»). При этом ip-адрес
 сетевого узла должен создаваться с помощью функции ip_address().
 """
+from subprocess import Popen, PIPE
+import socket
+
+
+def host_ping(pool_ip):
+    for item in pool_ip:
+        try:
+            ip = socket.gethostbyname(item)
+            proc = Popen(['ping', ip], stdout=PIPE)
+            code = proc.wait()
+            if code:
+                print(f'{item} --> недоступен')
+            else:
+                print(f'{item} --> доступен')
+        except socket.gaierror:
+            print(f'{item} --> адрес или имя хоста некорректно')
+
+
+hosts = ['google.com', '192.168.1111.1111', 'yandex.ru', '192.168.8.1']
+host_ping(hosts)
